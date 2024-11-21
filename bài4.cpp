491a8b7d64cd2c4b56b5641620c9d7a1b367e1ca
KimiPrime <kingsan049@gmail.com>
2024-11-22 00:26:51 +0700

    Add files via upload

--- bài4.cpp
+++ bài4.cpp
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    // Khai báo các bi?n cho di?m môn Toán, Van, Anh
     float diemToan, diemVan, diemAnh;
     
-    // Yêu c?u ngu?i dùng nh?p di?m
     printf("Nhap diem Toan: ");
+    
     scanf("%f", &diemToan);
     
     printf("Nhap diem Van: ");
+    
     scanf("%f", &diemVan);
     
     printf("Nhap diem Anh: ");
+    
     scanf("%f", &diemAnh);
     
-    // Tính t?ng di?m và di?m trung bình
     float tongDiem = diemToan + diemVan + diemAnh;
+    
     float diemTrungBinh = tongDiem / 3;
     
-    // In k?t qu? ra màn hình
     printf("Tong diem: %.2f\n", tongDiem);
+    
     printf("Diem trung binh: %.2f\n", diemTrungBinh);
     
     return 0;
