f5309dd6e8e57bd7707945557383ecab032997cb
KimiPrime <kingsan049@gmail.com>
2024-11-22 00:25:34 +0700

    Add files via upload

--- bài4.cpp
+++ bài4.cpp
@@ -1,10 +1,26 @@
 #include <stdio.h>
-#define PI 3.14
 
 int main() {
-    float r;
-    printf("Nhap r: ");
-    scanf("%f", &r);
-    printf("Chu vi: %.2f\nDien tich: %.2f\n", 2 * PI * r, PI * r * r);
+    // Khai báo các bi?n cho di?m môn Toán, Van, Anh
+    float diemToan, diemVan, diemAnh;
+    
+    // Yêu c?u ngu?i dùng nh?p di?m
+    printf("Nhap diem Toan: ");
+    scanf("%f", &diemToan);
+    
+    printf("Nhap diem Van: ");
+    scanf("%f", &diemVan);
+    
+    printf("Nhap diem Anh: ");
+    scanf("%f", &diemAnh);
+    
+    // Tính t?ng di?m và di?m trung bình
+    float tongDiem = diemToan + diemVan + diemAnh;
+    float diemTrungBinh = tongDiem / 3;
+    
+    // In k?t qu? ra màn hình
+    printf("Tong diem: %.2f\n", tongDiem);
+    printf("Diem trung binh: %.2f\n", diemTrungBinh);
+    
     return 0;
 }
